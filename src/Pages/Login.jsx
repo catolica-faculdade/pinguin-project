f1ea6f32b07a6f5ca6badf612af2840e29209295
Levirroh <johann.gr14@gmail.com>
2025-04-08 22:18:28 -0300

    adicionando estrutura inicial da pagina de Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,32 @@
 function Login(){
     // conversa com a api aqui
 
-
-
     return (
         // retorna no 'html' pela main aqui
-        <div>
-            <h1>Página de Login</h1>
-            <p>Oi</p>
-        </div>
+        <section className="zinc-200 flex">
+            <div className="neutral-100 flex">
+                <div className="">
+                    <div>
+                        <p>Preencha seus dados, por gentileza</p>
+                    </div>
+                    <div>
+                        <input type="text" value='Email ou nome de usuário'/>
+                        <input type="text" value='Insira sua senha'/>
+                    </div>
+                    <div>
+                        <button>Entre agora mesmo!</button>
+                    </div>
+                </div>
+                <div className="bg-purple-700">
+                    <div className="text-white">
+                        <h1>Bem-vindo de volta</h1>
+                    </div>
+                    <div>
+                        <img src="src\assets\images\logo-pinguIn-branca.png" alt="logo da empresa" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
