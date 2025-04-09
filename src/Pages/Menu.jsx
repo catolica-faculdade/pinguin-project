4cb81cd5ea037435980286b12fe7d61fb40b494d
gfloriano11 <gflorianodev@gmail.com>
2025-04-08 22:16:04 -0300

    feat: iniciar menu page

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -4,8 +4,14 @@ function Menu(){
 
     return(
         <div>
-            <p>bem-vindo!</p>
-            <Button/>
+            <div className='sm:flex'>
+                <img className='w-40 sm:w-50'
+                src='src/assets/images/logo-pinguIn-roxo.png'></img>
+                <div className='flex font-inter'>
+                    <p>Bem-vindo ao Pingu</p>
+                    <p className='purple-pinguin font-bold'>In</p>
+                </div>
+            </div>
         </div>
     );
 
