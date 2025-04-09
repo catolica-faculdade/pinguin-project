479ad6fd1113af55efc8106fdcabb794acaf2995
gfloriano11 <gflorianodev@gmail.com>
2025-04-08 21:36:43 -0300

    feat: criar componente botao

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+function Button(){
+
+    return(
+        <Link className="text-red-500">
+            <p>botao</p>
+        </Link>
+    );
+}
+
+export default Button;
