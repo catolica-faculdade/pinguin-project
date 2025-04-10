f35deefd1f8c7fa3c522f58f04de08fb2214ff38
gfloriano11 <gflorianodev@gmail.com>
2025-04-09 21:55:37 -0300

    style: menu page

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -3,13 +3,16 @@ import Button from '../components/Button';
 function Menu(){
 
     return(
-        <div>
-            <div className='flex flex-col sm:flex-row'>
+        <div className='flex justify-center items-center h-dvh bg-menu-pinguin'>
+            <div className='flex flex-col sm:flex-row sm:gap-10 justify-center items-center'>
                 <img className='w-40 sm:w-50'
                 src='src/assets/images/logo-pinguIn-roxo.png'></img>
-                <div className='flex font-inter'>
-                    <p>Bem-vindo ao Pingu</p>
-                    <p className='purple-pinguin font-bold'>In</p>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <div className='flex font-inter text-4xl'>
+                        <p>Bem-vindo ao Pingu</p>
+                        <p className='purple-pinguin font-bold'>In</p>
+                    </div>
+                    <div className='flex bg-purple-pinguin w-76 sm:w-96 h-2'></div>
                 </div>
             </div>
         </div>
