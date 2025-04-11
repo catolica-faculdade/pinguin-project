5685f53a839132a2f08558252bb7ed65afaff06d
gfloriano11 <gflorianodev@gmail.com>
2025-04-11 17:36:13 -0300

    fix: add pinguin logo inside div

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -5,8 +5,10 @@ function Menu(){
     return(
         <div className='flex justify-center items-center h-dvh bg-menu-pinguin'>
             <div className='flex flex-col sm:flex-row sm:gap-10 justify-center items-center'>
-                <img className='w-40 sm:w-50'
-                src='src/assets/images/logo-pinguIn-roxo.png'></img>
+                <div className='select-none pointer-events-none'>
+                    <img className='w-40 sm:w-50'
+                    src='src/assets/images/logo-pinguIn-roxo.png'></img>
+                </div>
                 <div className='flex flex-col justify-center items-center gap-2'>
                     <div className='flex font-inter text-4xl'>
                         <p>Bem-vindo ao Pingu</p>
