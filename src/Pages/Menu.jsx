3af3f5b7f61a99d5fcac6723d25e987571fe361f
gfloriano11 <gflorianodev@gmail.com>
2025-04-11 17:46:03 -0300

    style: decreased font size for mobile

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -10,11 +10,11 @@ function Menu(){
                     src='src/assets/images/logo-pinguIn-roxo.png'></img>
                 </div>
                 <div className='flex flex-col justify-center items-center gap-2'>
-                    <div className='flex font-inter text-4xl'>
+                    <div className='flex font-inter text-3xl sm:text-4xl'>
                         <p>Bem-vindo ao Pingu</p>
                         <p className='purple-pinguin font-bold'>In</p>
                     </div>
-                    <div className='flex bg-purple-pinguin w-76 sm:w-96 h-2'></div>
+                    <div className='flex bg-purple-pinguin w-78 sm:w-96 h-2'></div>
                 </div>
             </div>
         </div>
