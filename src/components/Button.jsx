9f09cd4f0f780dc5904d3a1fc1b70263e0c11e9f
gfloriano11 <gflorianodev@gmail.com>
2025-04-15 20:19:58 -0300

    chore: adicionar borda cinza

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 
-function Button({color, bgColor, text}){
+function Button({color, bgColor, text, border}){
 
     return(
-        <Link className={`flex ${bgColor} ${color} pt-2 pr-4 pb-2 pl-4 rounded-lg `}>
+        <Link className={`flex ${bgColor} ${color} ${border}
+        pt-2 pr-4 pb-2 pl-4 rounded-lg `}>
             <p>{text}</p>
         </Link>
     );
