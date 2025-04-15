637e3e11ef053e009f87ece5f05b7d31825f2919
Levirroh <johann.gr14@gmail.com>
2025-04-15 20:31:58 -0300

    update: new update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 function Input({text, image, type, placeholder}){
 
     return(
-        <div className="bg-white rounded-lg border border-gray-400 "> 
-            <img src={image} alt={image} />
-            <input className={`flex pt-2 pr-4 pb-2 pl-4 rounded-lg color text-black placeholder-black`} placeholder={placeholder} text={text} type={type}/>
+        <div className="bg-white rounded-lg border border-gray-400 flex items-center pl-3"
+        > 
+            <div className="bg-purple-pinguin flex items-center justify-center w-full h-8 rounded-4xl">
+
+                <img src={image} alt={image} className="h-4/7"/>
+
+            </div>
+
+            <input className={`flex pt-2 pr-4 pb-2 rounded-lg color text-black placeholder-black`} placeholder={'  '+placeholder} text={text} type={type}/>
+
         </div>
     );
 }
