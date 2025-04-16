45700118513edc7ffb8a3abbbc730b5ee8aad0d7
Gabriel Bosco <160626981+gabrielgbosco@users.noreply.github.com>
2025-04-15 21:41:18 -0300

    Alterando src de imagens da home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,23 +7,23 @@ function Home() {
             <section className="nav-bar-background">
                 <nav className="nav-bar">
                     <div className="logo-nav-bar">
-                        <img src="../styles/images/logo-pinguin-branco-fundo-cinza.svg" alt="Logo do PinguIn"/>
+                        <img src="src/assets/images/logo-pinguin-branco-fundo-cinza.svg" alt="Logo do PinguIn"/>
                     </div>
                     <div className="source-bar">
                         <input type="text" name="barra-de-pesquisa" id="barra-de-pesquisa" placeholder="Pesquisar..."/>
                     </div>
                     <div className="nav-bar-buttons">
                         <a href="./homepage.html" id="home-button">
-                            <img src="../styles/images/home-icon.svg" alt="Ir para página inicial"/>
+                            <img src="src/assets/images/home-icon.svg" alt="Ir para página inicial"/>
                                 <p>Início</p>
                         </a>
                         <a href="#" id="notifications-button">
-                            <img src="../styles/images/notifications-icon.svg" alt="Ir para notificações"/>
+                            <img src="src/assets/images/notifications-icon.svg" alt="Ir para notificações"/>
                                 <p>Notificações</p>
                         </a>
                         <button id="user-options-button">
-                            <img className="user-picture" src="../styles/images/profile-picture.svg" alt="Opções do usuário" />
-                            <img id="arrow-bottom" src="../styles/images/seta-para-baixo.svg" alt="Opções do usuário" />
+                            <img className="user-picture" src="src/assets/images/profile-picture.svg" alt="Opções do usuário" />
+                            <img id="arrow-bottom" src="src/assets/images/seta-para-baixo.svg" alt="Opções do usuário" />
                         </button>
                     </div>
                 </nav>
@@ -33,14 +33,14 @@ function Home() {
                     <div className="first-sidebar-division">
                         <a className="following-followers" href="#">
                             <div>
-                                <img src="../styles/images/followers-icon.svg" alt="Seguindo"/>
+                                <img src="src/assets/images/followers-icon.svg" alt="Seguindo"/>
                                     <p>Seguindo</p>
                             </div>
                             <p className="number-followers-following">100</p>
                         </a>
                         <a className="following-followers" href="#">
                             <div>
-                                <img src="../styles/images/followers-icon.svg" alt="Seguidores"/>
+                                <img src="src/assets/images/followers-icon.svg" alt="Seguidores"/>
                                     <p>Seguidores</p>
                             </div>
                             <p className="number-followers-following">200</p>
@@ -48,7 +48,7 @@ function Home() {
                     </div>
                     <div className="second-sidebar-division">
                         <div className="this-user-datas">
-                            <img className="user-picture" src="../styles/images/profile-picture.svg" alt="Foto de perfil" />
+                            <img className="user-picture" src="src/assets/images/profile-picture.svg" alt="Foto de perfil" />
                             <div>
                                 <p>@gfloriano</p>
                                 <p>Gustavo Floriano</p>
@@ -66,26 +66,26 @@ function Home() {
                     <div className="post-card">
                         <div className="post-header">
                             <div className="this-user-datas">
-                                <img className="user-picture" src="../styles/images/profile-picture.svg" alt="Foto de perfil" />
+                                <img className="user-picture" src="src/assets/images/profile-picture.svg" alt="Foto de perfil" />
                                 <div>
                                     <p>@gfloriano</p>
                                     <p>Gustavo Floriano</p>
                                 </div>
                             </div>
                             <button>
-                                <img src="../styles/images/post-options-icon.svg" alt="Opções da postagem" />
+                                <img src="src/assets/images/post-options-icon.svg" alt="Opções da postagem" />
                             </button>
                         </div>
                         <p>Fui na casa do meu amigo...</p>
                         <button>Ver mais...</button>
                         <br/>
                             <div className="div-post-image">
-                                <img src="../styles/images/20170901casas-bonitas-62.jpg" alt="Imagem publicada" />
+                                <img src="src/assets/images/20170901casas-bonitas-62.jpg" alt="Imagem publicada" />
                             </div>
                             <div className="post-reactions">
                                 <div className="div-post-likes">
                                     <button>
-                                        <img src="../styles/images/like-heart-white.svg" alt="Curtir publicação"/>
+                                        <img src="src/assets/images/like-heart-white.svg" alt="Curtir publicação"/>
                                     </button>
                                     <button>
                                         +10M
@@ -93,7 +93,7 @@ function Home() {
                                 </div>
                                 <div className="div-post-comments">
                                     <button>
-                                        <img src="../styles/images/post-comment-icon.svg" alt="Curtir publicação"/>
+                                        <img src="src/assets/images/post-comment-icon.svg" alt="Curtir publicação"/>
                                     </button>
                                     <button>
                                         +100K
@@ -105,7 +105,7 @@ function Home() {
                 <div className="sidebar-features right-features">
                     <div className="streak-card">
                         <button className="streak-days-div openStreakPopup">
-                            <img src="../styles/images/streak-0-days.svg" alt="Streak"/>
+                            <img src="src/assets/images/streak-0-days.svg" alt="Streak"/>
                                 <p>0</p>
                         </button>
                         <div className="streak-description-today">
