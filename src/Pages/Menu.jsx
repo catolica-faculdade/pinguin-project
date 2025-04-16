e5a66141234aa1796cc2fd398c3aa8acc072e4d1
gfloriano11 <gflorianodev@gmail.com>
2025-04-15 22:18:46 -0300

    feat: pagina menu criada

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -4,45 +4,49 @@ function Menu(){
 
     return(
         <div className='flex justify-center gap-20 items-center h-dvh bg-menu-pinguin'>
-            <div className='flex flex-col gap-3'>
-                <div className='flex flex-col sm:flex-row sm:gap-10 justify-center items-center'>
-                    <div className='select-none pointer-events-none'>
-                        <img className='w-40 sm:w-50'
-                        src='src/assets/images/logo-pinguIn-roxo.png'></img>
+            <div className='flex flex-col flex-grow h-full'>
+                <div className='flex flex-col flex-grow sm:gap-5 gap-2 justify-center items-center'>
+                    <div className='flex flex-col sm:flex-row sm:gap-10 justify-center items-center'>
+                        <div className='select-none pointer-events-none'>
+                            <img className='w-40 sm:w-50'
+                            src='src/assets/images/logo-pinguIn-roxo.png'></img>
+                        </div>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <div className='flex font-inter text-3xl sm:text-4xl'>
+                                <p>Bem-vindo ao Pingu</p>
+                                <p className='purple-pinguin font-bold'>In</p>
+                            </div>
+                            <div className='flex bg-purple-pinguin w-78 sm:w-96 h-2'></div>
+                            <div className='hidden sm:flex flex-col justify-center items-center w-fit text-xl font-inter'>
+                                <p>Compartilhe momentos, conheça</p>
+                                <p>novas pessoas e faça parte de</p>
+                                <p>uma comunidade incrível.</p>
+                            </div>
+                            <div className='flex sm:hidden flex-col justify-center items-center w-60 text-lg font-inter'>
+                                <p>Crie.</p>
+                                <p>Compartilhe.</p>
+                                <p>Conecte-se.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex flex-col justify-center items-center gap-2'>
-                        <div className='flex font-inter text-3xl sm:text-4xl'>
-                            <p>Bem-vindo ao Pingu</p>
-                            <p className='purple-pinguin font-bold'>In</p>
+                    <div className='flex flex-col sm:flex-row sm:justify-around justify-center items-center gap-2 sm:gap-30 max-w-180'>
+                        <div className='w-45'>
+                            <Button
+                            border='border-solid gray-border border-1'
+                            bold='font-semibold'
+                            bgColor='bg-white'
+                            color='text-black'
+                            text='Registre-se aqui!'/>
                         </div>
-                        <div className='flex bg-purple-pinguin w-78 sm:w-96 h-2'></div>
-                        <div className='hidden sm:flex flex-col justify-center items-center w-fit text-xl font-inter'>
-                            <p>Compartilhe momentos, conheça</p>
-                            <p>novas pessoas e faça parte de</p>
-                            <p>uma comunidade incrível.</p>
-                        </div>
-                        <div className='flex sm:hidden flex-col justify-center items-center w-60 text-lg font-inter'>
-                            <p>Crie.</p>
-                            <p>Compartilhe.</p>
-                            <p>Conecte-se.</p>
+                        <div className='w-45 z-20'>
+                            <Button
+                            bgColor='bg-purple-pinguin'
+                            color='text-white'
+                            text='Entre agora mesmo!'/>
                         </div>
                     </div>
                 </div>
-                <div className='flex justify-around'>
-                    <div>
-                        <Button
-                        border='border-solid gray-border border-1'
-                        bgColor='bg-white'
-                        color='text-black'
-                        text='Registre-se aqui!'/>
-                    </div>
-                    <div>
-                        <Button
-                        bgColor='bg-purple-pinguin'
-                        color='text-white'
-                        text='Entre agora mesmo!'/>
-                    </div>
-                </div>
+                <div className="w-full h-32 bg-[url('/src/assets/images/mountains.png')] bg-no-repeat bg-top bg-cover"></div>
             </div>
         </div>
     );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
 
-function Button({color, bgColor, text, border}){
+function Button({color, bgColor, text, border, bold}){
 
     return(
         <Link className={`flex justify-center ${bgColor} ${color} ${border}
         pt-2 pr-4 pb-2 pl-4 rounded-lg `}>
-            <p>{text}</p>
+            <p className={`${bold}`}>{text}</p>
         </Link>
     );
 }
