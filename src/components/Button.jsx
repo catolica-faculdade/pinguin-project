f5529993aa0d35d4627fadad89482adf8abb38df
gfloriano11 <gflorianodev@gmail.com>
2025-04-22 17:46:44 -0300

    feat: add path in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 
-function Button({color, bgColor, text, border, bold}){
+function Button({color, bgColor, text, border, bold, path}){
 
     return(
-        <Link className={`flex justify-center ${bgColor} ${color} ${border}
+        <Link
+        to={path} 
+        className={`flex justify-center ${bgColor} ${color} ${border}
         pt-2 pr-4 pb-2 pl-4 rounded-lg `}>
             <p className={`${bold}`}>{text}</p>
         </Link>
