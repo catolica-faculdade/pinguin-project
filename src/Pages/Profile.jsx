ae7822675ac33d50e84697e4aa89ff60895c9d3a
gfloriano11 <gflorianodev@gmail.com>
2025-04-24 20:12:55 -0300

    style: add padding top to separate navbar from website

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -8,7 +8,18 @@ function Profile() {
         // retorna no 'html' pela main aqui
         <section>
             <NavBar />
-            
+            <section className='flex bg-user-icon pt-[10vh]'>
+                <div className='flex bg-icon-user'>
+                    <img className='w-20 h-20' 
+                    src='/src/assets/images/user-icon-black.png'></img>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </section>
         </section>
     );
 };
