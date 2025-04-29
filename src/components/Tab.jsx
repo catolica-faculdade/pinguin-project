e0f282ce7bb083ae5bd22af09169974201724004
gfloriano11 <gflorianodev@gmail.com>
2025-04-29 19:31:16 -0300

    feat: tab component

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,7 +1,21 @@
-function Tab(){
+function Tab({text, number}){
     return(
-        <div>
-            <p>oi</p>
+        <div className="flex items-center pl-2 h-14 gap-2 bg-tab rounded-[18px] min-w-60">
+            <div>
+                <img
+                className="w-12"
+                src="/src/assets/images/followers-icon.svg"></img>
+            </div>
+            <div className="flex justify-between w-full pr-4">
+                <div className="flex items-center">
+                    <p>{text}</p>
+                </div>
+                <div>
+                    <p>{number}</p>
+                </div>
+            </div>
         </div>
     );
 }
+
+export default Tab;
