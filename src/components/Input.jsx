363cfea5ee7391ad940cc091f9170cd57267fe0b
Levirroh <johann.gr14@gmail.com>
2025-04-29 20:11:58 -0300

    fix: input com width automatica errada

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,10 +6,10 @@ function Input({text, image, type, placeholder, size}){
     return(
         <div className="bg-white rounded-lg border border-gray-400 flex items-center pl-3 max-w-100 w-60"
         > 
-            <div className="bg-purple-pinguin flex items-center justify-center w-9 md:h-7 h-7 rounded-4xl">
-                <img src={image} alt={image} className={`h-[${size}%]`}/>
+            <div className="bg-purple-pinguin flex items-center justify-center w-9 h-7 rounded-4xl">
+                <img src={image} alt={image} className={`h-[${size}%] w-[70%]`}/>
             </div>
-            <input className={`w-[100%] pt-2 pb-2 rounded-lg color text-black placeholder-black`} placeholder={'  '+placeholder} text={text} type={type}/>
+            <input className={`w-[100%] pt-2 pb-2 pl-2 rounded-lg color text-black placeholder-black`} placeholder={placeholder} text={text} type={type}/>
         </div>
     );
 }
