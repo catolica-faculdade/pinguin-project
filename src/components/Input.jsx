ae59c0d6621214e39435c06fb74178dcd221055d
Levirroh <johann.gr14@gmail.com>
2025-04-29 20:06:35 -0300

    feat: novas funcionalidades no input e responsabilidade

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,15 @@
 import { Link } from "react-router-dom";
 
 
-function Input({text, image, type, placeholder}){
+function Input({text, image, type, placeholder, size}){
 
     return(
-        <div className="bg-white rounded-lg border border-gray-400 flex items-center pl-3"
+        <div className="bg-white rounded-lg border border-gray-400 flex items-center pl-3 max-w-100 w-60"
         > 
-            <div className="bg-purple-pinguin flex items-center justify-center w-full h-8 rounded-4xl">
-
-                <img src={image} alt={image} className="h-4/7"/>
-
+            <div className="bg-purple-pinguin flex items-center justify-center w-9 md:h-7 h-7 rounded-4xl">
+                <img src={image} alt={image} className={`h-[${size}%]`}/>
             </div>
-
-            <input className={`flex pt-2 pr-4 pb-2 rounded-lg color text-black placeholder-black`} placeholder={'  '+placeholder} text={text} type={type}/>
-
+            <input className={`w-[100%] pt-2 pb-2 rounded-lg color text-black placeholder-black`} placeholder={'  '+placeholder} text={text} type={type}/>
         </div>
     );
 }
