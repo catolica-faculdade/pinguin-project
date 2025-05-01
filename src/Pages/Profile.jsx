e3f035b739db8eb1001dbabd7d45b8ca7367064d
gfloriano11 <gflorianodev@gmail.com>
2025-04-30 21:11:43 -0300

    fix: import left and right menu

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,43 +1,21 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import Tab from '../components/Tab';
-import MyUser from '../components/MyUser';
-import Streak from '../components/Streak';
+import LeftMenu from '../components/LeftMenu';
+import RightMenu from '../components/RightMenu';
 
 function Profile() {
     // conversa com a api aqui
-
-    const following = 20;
-    const followers = 15;
-    const username = '@gfloriano';
-    const userFullname = 'Gustavo Floriano';
 
     return (
         // retorna no 'html' pela main aqui
         <section>
             <NavBar />
             <section className='flex bg-user-icon pt-[8vh] h-dvh'>
-                <div className='flex flex-col gap-3 justify-between items-center pt-4 p-2 border-black border-r'> 
-                    <div className='flex flex-col gap-3'>
-                        <Tab 
-                        text="Following"
-                        number={following}/>
-                        <Tab 
-                        text="Followers"
-                        number={followers}/>
-                    </div>
-                    <MyUser
-                    username={username}
-                    userFullname={userFullname}/>
-                </div>
+            <LeftMenu/>
                 <div>
                     
                 </div>
-                <div className='flex flex-col gap-3 justify-between items-center pt-4 p-2 border-black border-l'>
-                    <div>
-                        <Streak/>
-                    </div>
-                </div>
+            <RightMenu/>
             </section>
         </section>
     );
