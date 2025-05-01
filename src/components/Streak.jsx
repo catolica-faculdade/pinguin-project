23ea6e3928b084fb6a3cea93ad9ab0386a49270b
gfloriano11 <gflorianodev@gmail.com>
2025-04-30 21:07:09 -0300

    style: my user component

--- src/components/Streak.jsx
+++ src/components/Streak.jsx
@@ -3,15 +3,18 @@ function Streak(){
     const streak = 0;
 
     return(
-        <div className="flex justify-center items-center bg-navbar rounded-[18px] max-w-75 p-2 gap-2">
-            <div className="flex items-center">
-                <img className="w-30" src="/src/assets/images/streak-0-days.svg"></img>
+        <div className="flex justify-center items-center bg-navbar rounded-[18px] max-w-72 p-2 gap-2
+        border-1 border-gray-700 shadow-2xl">
+            <div className="flex items-center pt-2 pointer-events-none select-none">
+                <img className="w-25" src="/src/assets/images/streak-0-days.svg"></img>
                 <p className="text-2xl font-bold">{streak}</p>
             </div>
             <div className="flex flex-col">
                 <p>Seu streak é de {streak} dias.</p>
                 <div className="flex-1">
-                    <p>Crie seu três posts e inicie o streak!<button><p>Criar publicação</p></button></p>
+                    <p>Crie seu três posts e inicie o streak!
+                        <button className="pl-1 underline text-purple-800 font-bold">Criar publicação</button>
+                    </p>
                 </div>
             </div>
         </div>
