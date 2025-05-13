9c16aee2524d63de87b316d1330dbae2fdda8b43
gfloriano11 <gflorianodev@gmail.com>
2025-05-13 17:56:22 -0300

    feat: add prfoile info numbers

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -8,6 +8,9 @@ function Profile() {
     const username = '@gfloriano';
     const userFullname = 'Gustavo Floriano';
     const streak = 0;
+    const postsNumber = 10;
+    const followers = 19;
+    const following = 23;
 
 
     return (
@@ -17,7 +20,7 @@ function Profile() {
             <section className='flex bg-user-icon pt-[8vh] h-dvh'>
             <LeftMenu/>
             <div className='w-full md:w-2/3 p-7'>
-                <div className='user-header'>
+                <div className='flex flex-col gap-3'>
                     <div className='flex justify-between'>
                         <div className='flex items-center gap-3'>
                             <div className='pointer-events-none select-none w-18'>
@@ -43,6 +46,11 @@ function Profile() {
                                 <img src='src/assets/images/configs-icon.svg'></img>
                             </div>
                         </div>
+                    </div>
+                    <div className='flex gap-3'>
+                        <div className='flex gap-1'> <p className='font-bold'>{postsNumber}</p> <p>Posts</p> </div>
+                        <div className='flex gap-1'> <p className='font-bold'>{followers}</p> <p>Followers</p> </div>
+                        <div className='flex gap-1'> <p className='font-bold'>{following}</p> <p>Following</p> </div>
                     </div>
                 </div>
                 <div className='posts'>
