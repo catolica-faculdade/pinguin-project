f1658286988d6bfd58d923c5bdbddb233c25f4f5
Gabriel Bosco <160626981+gabrielgbosco@users.noreply.github.com>
2025-05-20 20:51:44 -0300

    Responsividade e nav Home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 
 export default function NavBar() {
-  return (
-    <nav className="flex justify-between bg-navbar p-1 items-center fixed w-full h-[8vh] border-b">
-      <div className="pl-[10px]">
-          <div className="w-[42px] h-[42px] bg-cube-gray-navbar rounded-[100px] p-[4px]">
-              <img src='src/assets/images/logo-pinguIn-branca.png'/>
-          </div>
-      </div>
-      <div className="pl-[15vh]">
-          <input className="bg-input-navbar rounded-[4px] pl-[10px] w-[40vh] h-[28px] color-text-search-bar outline-0"
-          placeholder="Pesquisar..." type="text" />
-      </div>
-      <div className="flex pr-[30px] items-center">
-          <div className="flex flex-col items-center">
-              <img className="w-[25px] pt-[4px]" src='src/assets/images/house-icon.png'/>
-              <a className="text-[15px]">Início</a>
-          </div>
-          <div className="flex flex-col items-center pl-[30px]">
-              <img className="w-[25px]" src='src/assets/images/notifications-icon.png'/>
-              <a className="text-[15px]">Notificação</a>
-          </div>
-          <div className="flex items-center pl-[20px]">
-              <div className="rounded-[50%] p-[2px] items-center">
-                  <img className="w-[35px] min-w-[35px]" src='src/assets/images/profile-picture.svg'/>
-              </div>
-              <div>
-                  <img className="pl-[10px]" src='src/assets/images/down-arrow-icon.png'/>
-              </div>
-          </div>
-      </div>
-  </nav>
-  );
+    return (
+        <nav className="top-nav-bar-mobile flex justify-between bg-navbar p-1 items-center fixed w-full h-[8vh] border-b">
+            <div className="pl-[10px]">
+                <div className="w-[42px] h-[42px] bg-cube-gray-navbar rounded-[100px] p-[4px]">
+                    <img src='src/assets/images/logo-pinguIn-branca.png' />
+                </div>
+            </div>
+            <div className="pl-[15vh]">
+                <input className="bg-input-navbar rounded-[4px] pl-[10px] w-[40vh] h-[28px] color-text-search-bar outline-0"
+                    placeholder="Pesquisar..." type="text" />
+            </div>
+            <div className="flex pr-[30px] items-center">
+                <div className="flex flex-col items-center">
+                    <img className="w-[25px] pt-[4px]" src='src/assets/images/house-icon.png' />
+                    <a className="text-[15px]">Início</a>
+                </div>
+                <div className="flex flex-col items-center pl-[30px]">
+                    <img className="w-[25px]" src='src/assets/images/notifications-icon.png' />
+                    <a className="text-[15px]">Notificação</a>
+                </div>
+                <div className="flex items-center pl-[20px]">
+                    <div className="rounded-[50%] p-[2px] items-center">
+                        <img className="w-[35px] min-w-[35px]" src='src/assets/images/profile-picture.svg' />
+                    </div>
+                    <div>
+                        <img className="pl-[10px]" src='src/assets/images/down-arrow-icon.png' />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
--- src/components/NavBarMobile.jsx
+++ src/components/NavBarMobile.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export default function NavBarMobile() {
     return (
-        <nav className="flex justify-around bg-navbar p-1 items-center fixed bottom-0 w-full h-[10vh] border-t top-shadow-nav-bar-mobile">
+        <nav className="nav-bar-mobile flex justify-around bg-navbar p-1 items-center fixed bottom-0 w-full h-[60px] border-t top-shadow-nav-bar-mobile">
             <button>
-                <img src='src/assets/images/home-icon-mobile.svg' />
+                <img src='src/assets/images/home-icon-mobile.svg' className="w-[28px]" />
             </button>
             <button>
-                <img src='src/assets/images/search-icon-mobile.svg' />
+                <img src='src/assets/images/search-icon-mobile.svg' className="w-[28px]" />
             </button>
-            <button className="bg-purple-create-post rounded-md border p-2 relative -top-3">
+            <button className="bg-purple-create-post rounded-md border p-2 relative -top-4">
                 <img src='src/assets/images/create-post-icon-mobile.svg' />
             </button>
             <button>
-                <img src='src/assets/images/notifications-icon-mobile.svg' />
+                <img src='src/assets/images/notifications-icon-mobile.svg' className="w-[25px]" />
             </button>
             <button>
-                <img src='src/assets/images/profile-icon-mobile.svg' />
+                <img src='src/assets/images/profile-icon-mobile.svg' className="w-[25px]" />
             </button>
         </nav>
     );
