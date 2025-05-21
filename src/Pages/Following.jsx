a3ca23d525f542415fadb796fc179f3043f60a4e
gfloriano11 <gflorianodev@gmail.com>
2025-05-20 22:02:15 -0300

    feat: add left and right menu in profile page

--- src/Pages/Following.jsx
+++ src/Pages/Following.jsx
@@ -1,8 +1,15 @@
+import LeftMenu from "../components/LeftMenu";
+import RightMenu from "../components/RightMenu";
+
 function Following(){
 
     return (
         <div>
-            <p>oi</p>
+            <LeftMenu/>
+            <div>
+                <p>oi</p>
+            </div>
+            <RightMenu/>
         </div>
     )
 }
