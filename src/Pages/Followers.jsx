a0de26c633e47ed2b4e0718d1df997200f2ecc8a
gfloriano11 <gflorianodev@gmail.com>
2025-05-20 21:37:05 -0300

    feat: add follower/ing pages files

---
+++ src/Pages/Followers.jsx
@@ -0,0 +1,10 @@
+function Followers(){
+
+    return (
+        <div>
+            <p>oi</p>
+        </div>
+    )
+}
+
+export default Followers;
---
+++ src/Pages/Following.jsx
@@ -0,0 +1,10 @@
+function Following(){
+
+    return (
+        <div>
+            <p>oi</p>
+        </div>
+    )
+}
+
+export default Following;
