54b68ed09be812a408ff4486694d3913065e4663
gfloriano11 <gflorianodev@gmail.com>
2025-05-27 22:03:34 -0300

    feat: import components in following pages

--- src/Pages/Following.jsx
+++ src/Pages/Following.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import LeftMenu from "../components/LeftMenu";
 import RightMenu from "../components/RightMenu";
+import FollowTab from "../components/FollowTab";
 
 function Following(){
+
+    const following = [
+        {
+            username: 'gaucho',
+            fullname: 'João Constantino Caetano',
+        },
+        {
+            username: 'johanngr14',
+            fullname: 'Johann Gossen Ruth',
+        },
+        {
+            username: 'vh',
+            fullname: 'Vitor Hugo da Cunha',
+        },
+        {
+            username: 'gabbosco',
+            fullname: 'Gabriel Bosco',
+        }
+    ];
 
     return (
         <div className="flex">
             <LeftMenu/>
-            <div className="w-full">
-                <p>oi</p>
+            <div className="w-full md:w-3/5 flex flex-col overflow-y-scroll">
+                {/* <p>oi</p> */}
+                {following.map((user) => (
+                    // <p>oi</p>
+                    <FollowTab user={user}/>
+                ))}
             </div>
             <RightMenu/>
         </div>
