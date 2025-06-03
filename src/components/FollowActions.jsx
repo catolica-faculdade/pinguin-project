e1c1ac84d8a7ca8228dac344eed43e4f0175b33c
gfloriano11 <gflorianodev@gmail.com>
2025-06-03 17:47:27 -0300

    style: min width to follow actions

--- src/components/FollowActions.jsx
+++ src/components/FollowActions.jsx
@@ -1,6 +1,6 @@
 function FollowActions({color, text}){
     return(
-        <div className="flex justify-center items-center w-1/5">
+        <div className="flex justify-center items-center w-1/5 min-w-26">
             <button className={`${color} border-2 border-zinc-800 w-full rounded-md`}>
                 <p className="text-white text-sm">{text}</p>
             </button>
