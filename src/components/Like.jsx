9457b0654faeb6f7f7d3206bda5c8fc27d6a6a82
gfloriano11 <gflorianodev@gmail.com>
2025-06-07 22:07:48 -0300

    feat: like posts [frontend]

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,8 +1,20 @@
-function Like({like}){
+import { useState } from "react";
+
+function Like({likeNumber}){
+    
+    const [like, setLike] = useState(false);
+
     return(
         <div className="flex items-center gap-1">
-            <img src="src/assets/images/like-heart-white.svg"></img>
-            <p>{like}</p>
+            {like
+            ?
+            <img onClick={() => setLike(false)}
+            src="src/assets/images/like-heart-red.svg"></img>
+            :
+            <img onClick={() => setLike(true)}
+            src="src/assets/images/like-heart-white.svg"></img>
+            }
+            <p>{likeNumber}</p>
         </div>
     );
 }
