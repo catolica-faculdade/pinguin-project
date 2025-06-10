2bf31fa61d16511837adf90f1e3a0397622a586d
gfloriano11 <gflorianodev@gmail.com>
2025-06-10 20:06:14 -0300

    feat: create edit mode in profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import NavBar from '../components/NavBar';
 import LeftMenu from '../components/LeftMenu';
 import RightMenu from '../components/RightMenu';
-import Post from '../components/Post';
-import { Link } from 'react-router-dom';
+import ShowProfile from '../components/Profile/ShowProfile';
+import EditProfile from '../components/Profile/EditProfile';
 
 function Profile() {
+
+    const [editMode, setEditMode] = useState(false);
 
     const user = {
         username: '@gfloriano',
         userFullname: 'Gustavo Floriano',
+        about: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         streak: 0,
         postsNumber: 10,
         followers: 19,
@@ -33,46 +36,12 @@ function Profile() {
             <NavBar />
             <section className='flex bg-user-icon pt-[8vh] h-dvh'>
             <LeftMenu/>
-            <div className='w-full md:w-3/5 p-7 flex flex-col gap-6 overflow-y-scroll'>
-                <div className='flex flex-col gap-3'>
-                    <div className='flex justify-between'>
-                        <div className='flex items-center gap-3'>
-                            <div className='pointer-events-none select-none w-18'>
-                                <img className='w-full' src='/src/assets/images/profile-picture.svg'></img>
-                            </div>
-                            <div className='flex flex-col justify-start'>
-                                <div className='flex items-center pr-8'>
-                                    <p>{user.username}</p>
-                                    <img className='w-8' src='/src/assets/images/streak-0-days.svg'></img>
-                                    <p className='font-bold'>{user.streak}</p>
-                                </div>
-                                <p>{user.userFullname}</p>
-                            </div>
-                        </div>
-                        <div className='flex gap-x-4 items-start'>
-                            <div>
-                                <button className='hidden md:flex bg-navbar w-36 justify-center p-1 border-2 border-gray-600 rounded-xl
-                                hover:cursor-pointer'>
-                                    <p>Editar perfil</p>
-                                </button>
-                            </div>
-                            <Link to="/settings" className='w-8 cursor-pointer'>
-                                <img src='src/assets/images/configs-icon.svg'></img>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className='flex gap-3'>
-                        <div className='flex gap-1'> <p className='font-bold'>{user.postsNumber}</p> <p>Posts</p> </div>
-                        <Link to="/followers" className='flex gap-1'> <p className='font-bold'>{user.followers}</p> <p>Followers</p> </Link>
-                        <Link to="/following" className='flex gap-1'> <p className='font-bold'>{user.following}</p> <p>Following</p> </Link>
-                    </div>
-                </div>
-                {posts.map((post) => (
-                <div className='posts flex justify-center'>
-                    <Post user={user} content={post}/>
-                </div>
-                ))}
-            </div>
+            {editMode
+            ?
+                <EditProfile user={user} posts={posts} onClick={() => setEditMode(false)}/>
+            :
+                <ShowProfile user={user} posts={posts} onClick={() => setEditMode(true)}/>
+            }
             <RightMenu/>
             </section>
         </section>
---
+++ src/components/Profile/EditProfile.jsx
@@ -0,0 +1,52 @@
+import Post from '../../components/Post';
+import { Link } from 'react-router-dom';
+import ProfileButton from '../../components/ProfileButton';
+
+function EditProfile({user, posts, onClick}){
+    return(
+        <div className='w-full md:w-3/5 p-7 flex flex-col gap-6 overflow-y-scroll'>
+            <div className='flex flex-col gap-3'>
+                <div className='flex justify-between'>
+                    <div className='flex items-center gap-3'>
+                        <div className='pointer-events-none select-none w-18'>
+                            <img className='w-full' src='/src/assets/images/profile-picture.svg'></img>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <div className='flex items-center pr-8'>
+                                <p>{user.username}</p>
+                                <img className='w-8' src='/src/assets/images/streak-0-days.svg'></img>
+                                <p className='font-bold'>{user.streak}</p>
+                            </div>
+                            <p>{user.userFullname}</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-x-4 items-start'>
+                        <div>
+                            <ProfileButton onClick={onClick}
+                                text='Editar Perfil'
+                                color='bg-gray-600' />
+                        </div>
+                        <Link to="/settings" className='w-8 cursor-pointer'>
+                            <img src='src/assets/images/configs-icon.svg'></img>
+                        </Link>
+                    </div>
+                </div>
+                <div className='flex gap-3'>
+                    <div className='flex gap-1'> <p className='font-bold'>{user.postsNumber}</p> <p>Posts</p> </div>
+                    <Link to="/followers" className='flex gap-1'> <p className='font-bold'>{user.followers}</p> <p>Followers</p> </Link>
+                    <Link to="/following" className='flex gap-1'> <p className='font-bold'>{user.following}</p> <p>Following</p> </Link>
+                </div>
+                <div>
+                    <p>{user.about}</p>
+                </div>
+            </div>
+            {posts.map((post) => (
+                <div className='posts flex justify-center'>
+                    <Post user={user} content={post} />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default EditProfile;
