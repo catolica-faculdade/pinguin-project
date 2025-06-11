d5f68eb9583ae51149e4b6492aa648b9317550ff
Levirroh <johann.gr14@gmail.com>
2025-06-10 21:44:38 -0300

    fix: settings toggle switch

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -3,23 +3,41 @@ import RightMenu from "../components/RightMenu";
 import NavBar from "../components/NavBar";
 import TitleBar from "../components/TitleBar";
 import Setting from "../components/Setting";
+import { useState } from "react";
 
-function Settings(){
-    return(
+function SettingsPage() {
+    const [toggles, setToggles] = useState([
+        { id: 1, checked: false },
+        { id: 3, checked: false }
+    ]);
+
+    const handleToggle = (id) => {
+        setToggles((prev) =>
+        prev.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item
+        )
+        );
+    };
+
+    const getChecked = (id) => toggles.find((item) => item.id === id)?.checked ?? false;
+
+    return (
         <section>
-            <NavBar/>
-            <section className="flex h-dvh pt-[8vh] shadow-">
-                <LeftMenu/>
-                <div className="w-full md:w-3/5">
-                    <TitleBar text={"Configurações"}/>
-                    <Setting text={"Idioma"} toggle={true}/>
-                    <Setting text={"Idioma"} toggle={false}/>
-                    <Setting text={"Idioma"} toggle={true}/>
-                    </div>
-                <RightMenu/>
-            </section>
+        <NavBar />
+        <section className="flex h-dvh pt-[8vh]">
+            <LeftMenu />
+            <div className="w-full md:w-3/5">
+            <TitleBar text={"Configurações"} />
+            <Setting text={"Idioma"} toggle={true} id={1} checked={getChecked(1)} onToggle={handleToggle} />
+            <Setting text={"Notificações"} toggle={false} id={2} link="/notificacoes" />
+            <Setting text={"Modo escuro"} toggle={true} id={3} checked={getChecked(3)} onToggle={handleToggle} />
+            <Setting text={"Conta"} toggle={false} id={4} link="/conta" />
+            <Setting text={"Ajuda"} toggle={false} id={5} link="/ajuda" />
+            </div>
+            <RightMenu />
+        </section>
         </section>
     );
 }
 
-export default Settings;
+export default SettingsPage;
