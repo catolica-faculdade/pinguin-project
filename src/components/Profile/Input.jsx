6f19fb5753bf17276b86310b46839447d7856079
gfloriano11 <gflorianodev@gmail.com>
2025-06-11 09:30:12 -0300

    style: input component

--- src/components/Profile/Input.jsx
+++ src/components/Profile/Input.jsx
@@ -1,5 +1,18 @@
-function Input(){
-    
+function Input({type, value, width}){
+    const inputClass = `w-full bg-neutral-300 rounded-sm border-1 border-gray-800 pl-0.5`;
+    if(type === 'text'){
+        return(
+            <input value={value} 
+            className={inputClass}
+            type="text"></input>
+        )
+    }
+
+    if(type === 'textarea'){
+        return(
+            <textarea className="resize-none"></textarea>
+        )
+    }
 }
 
 export default Input;
