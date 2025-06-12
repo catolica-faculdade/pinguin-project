7d6d851d088dde002f85150409018eae7bb30194
Gabriel Bosco <160626981+gabrielgbosco@users.noreply.github.com>
2025-06-12 20:30:20 -0300

    Criação do component para Desconectar da conta

--- src/components/ExitPopUp.jsx
+++ src/components/ExitPopUp.jsx
@@ -1,13 +1,13 @@
 function ExitPopUp() {
     return (
         <section className="w-screen h-screen bg-exit-pop-up fixed top-0 left-0 flex justify-center items-center">
-            <div className="w-[50%] bg-[white]">
-                <p className="">Você tem certeza disso?</p>
-                <div>
-                    <button className="w-[50%]">
+            <div className="w-[50%] xl:w-[30%] 2xl:w-[25%] max-sm:w-[80%] xl:text-lg bg-[white] flex flex-col rounded-md">
+                <p className="h-[5rem] self-center content-center text-center">Você tem certeza disso?</p>
+                <div className="flex">
+                    <button className="w-[50%] bg-modal-red text-white border border-black rounded-bl-md h-[3rem]">
                         Sair
                     </button>
-                    <button className="w-[50%]">
+                    <button className="w-[50%] border border-black rounded-br-md h-[3rem] px-[1rem]">
                         Continuar na conta
                     </button>
                 </div>
