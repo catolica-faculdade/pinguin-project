7b6c93f1a0026a6369403828e663d155f64fae84
gfloriano11 <gflorianodev@gmail.com>
2025-06-17 10:45:03 -0300

    feat: add useNavigate to go back in previous page

--- src/components/General/TitleBar.jsx
+++ src/components/General/TitleBar.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function TitleBar({text}){
+
+    const navigate = useNavigate()
     return(
         <div className="flex h-21 p-1 bg-navbar justify-between items-center pl-4 pr-4 border-b">
-            <img src="src\assets\images\seta-para-baixo.svg" className="rotate-90 h-1/3" />
+            <img src="src\assets\images\seta-para-baixo.svg" className="rotate-90 h-1/3 cursor-pointer"
+            onClick={() => navigate(-1)}/>
             <h1 className="text-2xl">{text}</h1>
             <p></p>
         </div>
