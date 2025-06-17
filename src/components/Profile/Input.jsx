1518051cbb80218882db754c6cc051d950eb5027
gfloriano11 <gflorianodev@gmail.com>
2025-06-17 18:00:04 -0300

    feat: change input values

--- src/components/Profile/Input.jsx
+++ src/components/Profile/Input.jsx
@@ -1,16 +1,17 @@
-function Input({type, value, width}){
+function Input({type, value, width, onChange}){
     const inputClass = `w-full bg-neutral-300 rounded-sm border-1 border-gray-800 pl-0.5`;
     if(type === 'text'){
         return(
             <input value={value} 
             className={inputClass}
-            type="text"></input>
+            type="text"
+            onChange={onChange}></input>
         )
     }
 
     if(type === 'textarea'){
         return(
-            <textarea className="resize-none"></textarea>
+            <textarea className="resize-none" onChange={onChange}></textarea>
         )
     }
 }
