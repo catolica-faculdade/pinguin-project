fb72910cd4375f8a5618e264c9b786f0eb791a79
gfloriano11 <gflorianodev@gmail.com>
2025-06-19 16:42:22 -0300

    feat: add prop user

--- src/components/Profile/ShowProfile.jsx
+++ src/components/Profile/ShowProfile.jsx
@@ -2,8 +2,7 @@ import Post from '../../components/Posts/Post';
 import { Link } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
 
-function ShowProfile({posts, onClick}){
-    const user = posts[0].user;
+function ShowProfile({user, posts, onClick}){
     return (
         <div className='w-full md:w-3/5 p-7 flex flex-col gap-6 overflow-y-scroll'>
             <div className='flex flex-col gap-3'>
