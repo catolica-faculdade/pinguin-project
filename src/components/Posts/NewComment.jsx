2d7a784ad9d8a019fe44a128963db9ab2066622b
gfloriano11 <gflorianodev@gmail.com>
2025-06-24 20:55:45 -0300

    style: new comment components sizes

--- src/components/Posts/NewComment.jsx
+++ src/components/Posts/NewComment.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function NewComment({ comments, setComments, user, handleKeyDown, inputValue, setInputValue }) {
 
     return (
-        <div className="flex items-center gap-1 justify-between w-full rounded-b-2xl bg-navbar p-4 border">
+        <div className="flex items-center gap-1 justify-between w-full rounded-b-2xl bg-navbar p-4 pb-0 border">
             <textarea
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
