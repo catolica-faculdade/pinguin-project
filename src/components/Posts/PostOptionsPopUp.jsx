1469cb21911919669ae21b9499d6664ba2ff0463
Levirroh <johann.gr14@gmail.com>
2025-06-24 20:14:02 -0300

    fix: popUp

--- src/components/Posts/PostOptionsPopUp.jsx
+++ src/components/Posts/PostOptionsPopUp.jsx
@@ -5,18 +5,18 @@ function PostOptionsPopUp({setOpenOptions, openOptions}) {
     return (
         <div>
             {openOptions && (
-                <div className="bg-navbar w-28 border rounded-md absolute top-8 right-6">
+                <div className="bg-navbar w-28 border rounded-md absolute top-8 right-12">
                     <button className="w-full flex justify-between items-center px-1.5 py-1 border-b rounded-t-md">
                         <p>Fixar</p>
-                        <img src='src/assets/images/pin-icon.svg' className="h-[1rem]" />
+                        <img src='/src/assets/images/pin-icon.svg' className="h-[1rem]" />
                     </button>
                     <button className="w-full flex justify-between items-center px-1.5 py-1 border-b">
                         <p>Alterar</p>
-                        <img src='src\assets\images\pencil-empty-icon.svg' className="h-[1rem]" />
+                        <img src='/src\assets\images\pencil-empty-icon.svg' className="h-[1rem]" />
                     </button>
                     <button className="text-white w-full flex justify-between bg-modal-red items-center px-1.5 py-1 rounded-b-md">
                         <p>Deletar</p>
-                        <img src='src\assets\images\trash-white-icon.svg' className="h-[1rem]" />
+                        <img src='/src\assets\images\trash-white-icon.svg' className="h-[1rem]" />
                     </button>
                 </div>
             )}
