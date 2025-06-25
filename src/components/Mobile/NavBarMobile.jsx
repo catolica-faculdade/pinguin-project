4b77d530ceccdbdff61760cdd69d825233fe38f4
Levirroh <johann.gr14@gmail.com>
2025-06-25 19:35:52 -0300

    fix: adding navbar mobile e style da section

--- src/components/Mobile/NavBarMobile.jsx
+++ src/components/Mobile/NavBarMobile.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBarMobile() {
     return (
         <nav className="nav-bar-mobile flex justify-around bg-navbar p-1 md:hidden items-center fixed bottom-0 w-full h-[60px] border-t top-shadow-nav-bar-mobile">
-            <button>
+            <Link to={"/home"}>
                 <img src='src/assets/images/home-icon-mobile.svg' className="w-[28px]" />
-            </button>
-            <button>
+            </Link>
+            <Link to={"/search"}>
                 <img src='src/assets/images/search-icon-mobile.svg' className="w-[28px]" />
-            </button>
-            <button className="bg-purple-create-post rounded-md border p-2 relative -top-4">
+            </Link>
+            <Link to={"/newPost"} className="bg-purple-create-post rounded-md border p-2 relative -top-4">
                 <img src='src/assets/images/create-post-icon-mobile.svg' />
-            </button>
-            <button>
+            </Link>
+            <Link to={"/notifications"}>
                 <img src='src/assets/images/notifications-icon-mobile.svg' className="w-[25px]" />
-            </button>
-            <button>
+            </Link>
+            <Link to={"/profile"}>
                 <img src='src/assets/images/profile-icon-mobile.svg' className="w-[25px]" />
-            </button>
+            </Link>
         </nav>
     );
 }
